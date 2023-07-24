792c6aa8850d12f3cbd6d10a2fd6ee94d2d5a92f
YURIYKALYNBAEV <amber3200@mail.ru>
2023-07-24 23:30:44 +0500

    Решение задачи

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,7 +1,24 @@
 package com.gridnine.testing;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        List<Flight> flights = FlightBuilder.createFlights();
+        FlightFilterImpl flightFilter = new FlightFilterImpl();
+
+        System.out.println("Все рейсы:");
+        flightFilter.allFlightSegments(flights);
+        System.out.println("-----------------------------------");
+
+        System.out.println("Рейсы с вылетом раньше текущего времени:");
+        flightFilter.departureToTheCurrentPointInTime(flights);
+
+        System.out.println("Сегменты рейсов, дата прибытия которых предшествует дате вылета:");
+        flightFilter.thereAreSegmentsWithTheArrivalDateEarlierThanTheDepartureDate(flights);
+
+        System.out.println("Полеты, в которых общее время, проведённое на земле превышает два часа:");
+        flightFilter.theTotalTimeSpentOnTheGroundExceedsTwoHours(flights);
     }
 }
